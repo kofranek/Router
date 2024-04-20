17982b07d3244f8cf55996be432050aa61c47dc1
Jiří Kofránek <kofranek@gmail.com>
2024-04-20 23:07:20 +0200

    Přidána komponenta ExamplesView

--- hollarka_router/src/views/ExamplesView.vue
+++ hollarka_router/src/views/ExamplesView.vue
@@ -4,13 +4,11 @@ import examples from '../data.json'
 
 <template>
   <h1>Ukázky výukových simulátorů metabolismu železa</h1>
-  <div class="card">
-    <div v-for="example in examples" :key="example.id">
-      <div>
-        <a :href="example.html" target="_blank">
-          {{ example.title }}
-        </a>
-      </div>
+  <div v-for="example in examples" :key="example.id">
+    <div>
+      <a :href="example.html" target="_blank">
+        {{ example.title }}
+      </a>
     </div>
   </div>
 </template>
